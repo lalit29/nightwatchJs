1da99281490e8c4a253c6464627c0c87224d221d
ankithometown <ankit.patel@hometown.in>
2019-10-05 21:14:49 +0530

    test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,26 +5,27 @@ module.exports = {
   "src_folders" : ["tests"],
   'page_objects_path': ['page-objects'],
 
-  "selenium": {
-    "start_process": true,
-    "server_path": "drivers\\abc.jar",
-    "port": 4444,
-    // check_process_delay: 5000,
-    "cli_args": {
-    //  "webdriver.chrome.driver": "drivers\\chromedriver.exe",
-     "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
-    }
-  },
-  
-  // "webdriver" : {
+  // "selenium": {
   //   "start_process": true,
-  //   "server_path": "./node_modules/.bin/chromedriver",//for chrome
-  //   "port": 9515,
-  //   check_process_delay: 5000,
-  //   cli_args: [
-  //     '--port=9515'
-  //   ]
+  //   "server_path": "drivers\\abc.jar",
+  //   "port": 4444,
+  //   // check_process_delay: 5000,
+  //   "cli_args": {
+  //    "webdriver.chrome.driver": "drivers\\chromedriver.exe",
+  //   //  "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
+  //   }
   // },
+  
+  "webdriver" : {
+    "start_process": true,
+    // "server_path": "./node_modules/.bin/chromedriver",//for chrome
+    'server_path': require('chromedriver').path,
+    "port": 9515,
+    // check_process_delay: 5000,
+    // cli_args: [
+    //   '--port=9515'
+    // ]
+  },
 
   "globals_path": 'globals.js',
   
@@ -47,17 +48,7 @@ module.exports = {
         'path': 'tests_output/screenshots'
     },
 
-  //   "beta" : {
-  //     "globals" : {
-  //         "launchUrl" : "https://www.hometown.in/login?redirect=%2Fprofile"
-  //     }
-  // },
-  // "stage" : {
-  //     "globals" : {
-  //         "launchUrl" : "one_other_site",
-  //         "other_var" : "this env needs a different variable"
-  //     }
-  // },
+  
 
     "request_timeout_options": {
       "timeout": 300000,
@@ -67,28 +58,28 @@ module.exports = {
     
       "desiredCapabilities": {
 
-        "browserName": "firefox",
-        "moz:firefoxOptions": {
-          "args": ["--headless"]
-        },
+        // "browserName": "firefox",
+        // "moz:firefoxOptions": {
+        //   "args": ["--headless"]
+        // },
 
-      //   "browserName": "chrome",
-      //   //  "browserName" : "firefox",
-      //   "acceptSslCerts": true,
-      //   "acceptInsecureCerts": true,
-      //   'chromeOptions': {
-      //     w3c: false,
-      //     "args": [
-      //       "disable-web-security",
-      //       "ignore-certificate-errors",
-      //       "--test-type",
-      //       "--disable-gpu",
-      //       "--window-size=1860,1200",
-      //       "--headless",
-      //       // "--screenshot",
-      //       // "--dump-dom"
-      //     ],  
-      // },
+        "browserName": "chrome",
+        //  "browserName" : "firefox",
+        "acceptSslCerts": true,
+        "acceptInsecureCerts": true,
+        'chromeOptions': {
+          w3c: false,
+          "args": [
+            "disable-web-security",
+            "ignore-certificate-errors",
+            "--test-type",
+            "--disable-gpu",
+            "--window-size=1860,1200",
+            "--headless",
+            // "--screenshot",
+            // "--dump-dom"
+          ],  
+      },
 
       "disable-popup-blocking": true,
       "skip_testcases_on_fail": false,
