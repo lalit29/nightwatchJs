fd0ff6c5a345cc4097b9e11dd7da10190f3bd1e0
ankithometown <ankit.patel@hometown.in>
2019-10-06 01:44:41 +0530

    test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -19,7 +19,7 @@ module.exports = {
   "webdriver" : {
     "start_process": true,
     // "server_path": "./node_modules/.bin/chromedriver",//for chrome
-    "server_path": ".\\drivers\\chromedriver.exe",
+    "server_path": "./drivers/chromedriver.exe",
     // 'server_path': require('chromedriver').path,
     "port": 9515,
     // check_process_delay: 5000,
