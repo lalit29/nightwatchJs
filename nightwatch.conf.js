e81dee47d4c792ca2682f4aab56e62c758d5144e
ankithometown <ankit.patel@hometown.in>
2019-10-08 13:09:00 +0530

    change in driver and jar path

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,31 +5,32 @@ module.exports = {
   "src_folders" : ["tests"],
   'page_objects_path': ['page-objects'],
 
-  "selenium": {
-    "start_process": true,
-    // "server_path": "drivers\\abc.jar",
-    "server_path":argv.jar,
-    "port": 4444,
-    // check_process_delay: 5000,
-    "cli_args": {
-    //  "webdriver.chrome.driver": "drivers\\chromedriver.exe",
-     "webdriver.chrome.driver": argv.driver,
-     "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
-    }
-  },
-  
-  // "webdriver" : {
+  // "selenium": {
   //   "start_process": true,
-  //   // "server_path": "./node_modules/.bin/chromedriver",//for chrome
-  //   "server_path": "./drivers/chromedriver.exe",
-  //   // "server_path": "drivers/geckodriver.exe",
-  //   // 'server_path': require('geckodriver').path,
-  //   "port": 9515,
+  //   // "server_path": "drivers\\abc.jar",
+  //   "server_path":argv.jar,
+  //   "port": 4444,
   //   // check_process_delay: 5000,
-  //   // cli_args: [
-  //   //   '--port=9515'
-  //   // ]
+  //   "cli_args": {
+  //   //  "webdriver.chrome.driver": "drivers\\chromedriver.exe",
+  //    "webdriver.chrome.driver": argv.driver,
+  //    "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
+  //   }
   // },
+  
+  "webdriver" : {
+    "start_process": true,
+     "server_path": "./node_modules/.bin/chromedriver.exe",//for chrome
+     //'server_path': require('chromedriver').path,
+    //"server_path": "./drivers/chromedriver.exe",
+    // "server_path": "drivers/geckodriver.exe",
+    // 'server_path': require('geckodriver').path,
+    "port": 9515,
+    // check_process_delay: 5000,
+    // cli_args: [
+    //   '--port=9515'
+    // ]
+  },
 
   "globals_path": 'globals.js',
   
