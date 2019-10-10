368a92c378e7d1a18d7567c857f68650cb3bf5fc
Lalit Sukhwal <34391440+lalit29@users.noreply.github.com>
2019-10-10 13:30:18 +0530

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,34 +5,34 @@ module.exports = {
   "src_folders" : ["tests"],// test
   'page_objects_path': ['page-objects'],//
 
-  "selenium": {
-    "start_process": true,
-    // "server_path": "drivers\\abc.jar",//
-    "server_path": require("selenium-server").path,
-    //"server_path":argv.jar,
-    "port": 4444,
-    // check_process_delay: 5000,
-    "cli_args": {
-    //  "webdriver.chrome.driver": "drivers\\chromedriver.exe",
-      "webdriver.chrome.driver": require('chromedriver').path,
-    // "webdriver.chrome.driver": argv.driver,
-    // "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
-    }
-  },
-  
-//   "webdriver" : {
+//   "selenium": {
 //     "start_process": true,
-//      "server_path": "./node_modules/.bin/chromedriver.exe",//for chrome
-//      //'server_path': require('chromedriver').path,
-//     //"server_path": "./drivers/chromedriver.exe",
-//     // "server_path": "drivers/geckodriver.exe",
-//     // 'server_path': require('geckodriver').path,
-//     "port": 9515,
+//     // "server_path": "drivers\\abc.jar",//
+//     "server_path": require("selenium-server").path,
+//     //"server_path":argv.jar,
+//     "port": 4444,
 //     // check_process_delay: 5000,
-//     // cli_args: [
-//     //   '--port=9515'
-//     // ]
+//     "cli_args": {
+//     //  "webdriver.chrome.driver": "drivers\\chromedriver.exe",
+//       "webdriver.chrome.driver": require('chromedriver').path,
+//     // "webdriver.chrome.driver": argv.driver,
+//     // "webdriver.gecko.driver" : "drivers\\geckodriver.exe"
+//     }
 //   },
+  
+  "webdriver" : {
+    "start_process": true,
+    // "server_path": "./node_modules/.bin/chromedriver.exe",//for chrome
+     'server_path': require('chromedriver').path,
+    //"server_path": "./drivers/chromedriver.exe",
+    // "server_path": "drivers/geckodriver.exe",
+    // 'server_path': require('geckodriver').path,
+    "port": 9515,
+    // check_process_delay: 5000,
+    // cli_args: [
+    //   '--port=9515'
+    // ]
+  },
 
   "globals_path": 'globals.js',
   
